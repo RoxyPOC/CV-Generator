87e1ee566c65af51937caca7abbbcae3c0ba16ee
RoxyPOC <eleioan97@gmail.com>
2025-07-28 18:53:03 +0200

    Changes for .gitignore should not ignore the dist

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-// <<<<<<< HEAD
+  // <<<<<<< HEAD
   base: "/CV-Generator/",
-// =======
-  
-// >>>>>>> 77077529fd0b68cf5fc327e5b43b767fe848f3a1
+  // =======
+
+  // >>>>>>> 77077529fd0b68cf5fc327e5b43b767fe848f3a1
   plugins: [react()],
 });
